0e46638f6faba87e4db290e5e29d26336e309f2a
Rida Kodad <kodadreda@gmail.com>
2019-08-04 12:06:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(number.to_i)
-  index = number - 1  
+  index = number - 1
 end
 
 def move (board,position,character = "X")
